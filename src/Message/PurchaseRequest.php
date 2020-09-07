1da23ce015d2e17f2ef4966b4a7519adcba1d3f5
William Ning <wning@warp.local>
2020-09-08 08:52:11 +1200

    missed a comma

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -65,7 +65,7 @@ class PurchaseRequest extends AbstractRequest
             'x_customer_shipping_country'       => $card->getShippingCountry(),
             'x_customer_shipping_phone'         => $card->getShippingPhone(),
             'x_reference'                       => $this->getTransactionId(),
-            'x_shop_country'                    => 'NZ'
+            'x_shop_country'                    => 'NZ',
             'x_signature'                       => $this->getSignature(),
             'x_url_complete'                    => $returnUrl,
             'x_url_cancel'                      => $cancelUrl,
